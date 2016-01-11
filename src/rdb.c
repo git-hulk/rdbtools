33719eb880fa0d12d560cc547f9df81a4d3dca5b
git-hulk <hulk.website@gmail.com>
2016-01-11 23:48:52 +0800

    MOD: big refactor

--- src/rdb.c
+++ src/rdb.c
@@ -44,12 +44,19 @@
 #define REDIS_RDB_ENC_INT32 2
 #define REDIS_RDB_ENC_LZF 3
 
+#define KEY_FIELD_STR "key"
+#define VAL_FIELD_STR "value"
+#define TYPE_FIELD_STR "type"
+#define EXP_FIELD_STR "expire_time" 
+#define DB_NUM_STR "db_num"
+#define VERSION_STR "version"
+
 uint64_t cksum = 0;
  // NOTE: trick here, version set 5 as we want to calc crc where read version field.
 int version = 5;
 
-ssize_t    
-crc_read(int fildes, void *buf, size_t nbyte)
+static ssize_t    
+rdb_crc_read(int fildes, void *buf, size_t nbyte)
 {
     ssize_t len;
     len = read(fildes, buf, nbyte);
@@ -61,29 +68,27 @@ crc_read(int fildes, void *buf, size_t nbyte)
     return len;
 }
 
-/* =======================  RDB LOAD ====================== */
-uint8_t
-rdb_load_type(int fd)
+// ================================== READ DATA FROOM RDB FILE. ==================================== //
+static uint8_t
+rdb_read_kv_type(int fd)
 {
     char buf[1];
-    if(crc_read(fd, buf, 1) == 0) {
+    if(rdb_crc_read(fd, buf, 1) == 0) {
         logger(ERROR, "Exited, as read error on load type.\n");
-        exit(1);
     }
 
     return (uint8_t) buf[0];
 }
 
-// load string len
-uint32_t
-rdb_load_len(int fd, uint8_t *is_encoded)
+static uint32_t
+rdb_read_store_len(int fd, uint8_t *is_encoded)
 {
     char buf[2];
     uint8_t type;
     uint32_t len;
 
    if (is_encoded) *is_encoded = 0;
-    if (crc_read(fd, buf, 1) == 0) return REDIS_RDB_LENERR;
+    if (rdb_crc_read(fd, buf, 1) == 0) return REDIS_RDB_LENERR;
     type = (buf[0] & 0xc0) >> 6; 
 
     /**
@@ -95,10 +100,10 @@ rdb_load_len(int fd, uint8_t *is_encoded)
     if (REDIS_RDB_6B == type) {
         return (uint8_t)buf[0] & 0x3f;
     } else if (REDIS_RDB_14B == type) {
-        if (crc_read(fd, buf + 1, 1) == 0) return REDIS_RDB_LENERR;
+        if (rdb_crc_read(fd, buf + 1, 1) == 0) return REDIS_RDB_LENERR;
         return (((uint8_t)buf[0] & 0x3f) << 8)| (uint8_t)buf[1];
     } else if (REDIS_RDB_32B == type) {
-        if (crc_read(fd, &len, 4) == 0) return REDIS_RDB_LENERR;
+        if (rdb_crc_read(fd, &len, 4) == 0) return REDIS_RDB_LENERR;
         return ntohl(len); 
     } else {
         if(is_encoded) *is_encoded = 1;
@@ -106,28 +111,29 @@ rdb_load_len(int fd, uint8_t *is_encoded)
     }
 }
 
-int32_t
-rdb_load_int(int fd, uint8_t enc)
+static int32_t
+rdb_read_int(int fd, uint8_t enc)
 {
     char buf[4];
 
     if (REDIS_RDB_ENC_INT8 == enc) {
-        if (crc_read(fd, buf, 1) == 0) goto READERR;
+        if (rdb_crc_read(fd, buf, 1) == 0) goto READERR;
         return (int8_t)buf[0];
     } else if (REDIS_RDB_ENC_INT16 == enc) {
-        if (crc_read(fd, buf, 2) == 0) goto READERR;
+        if (rdb_crc_read(fd, buf, 2) == 0) goto READERR;
         return (int16_t)((uint8_t)buf[0] | ((uint8_t)buf[1] << 8));
     } else {
-        if (crc_read(fd, buf, 4) == 0) goto READERR;
+        if (rdb_crc_read(fd, buf, 4) == 0) goto READERR;
         return (int32_t)((uint8_t)buf[0] | ((uint8_t)buf[1] << 8) | ((uint8_t)buf[2] << 16) | ((uint8_t)buf[3] << 24));
     }
+
 READERR:
     logger(ERROR, "Exited, as read error on load integer.\n");
     exit(1);
 }
 
-char *
-rdb_load_lzf_string(int fd)
+static char *
+rdb_read_lzf_string(int fd)
 {
     int32_t clen, len;
     char *cstr, *str;
@@ -137,9 +143,9 @@ rdb_load_lzf_string(int fd)
      * 3. load lzf_string, and use lzf_decompress to decode.
      */
 
-    if((clen = rdb_load_len(fd, NULL)) == REDIS_RDB_LENERR)
+    if((clen = rdb_read_store_len(fd, NULL)) == REDIS_RDB_LENERR)
         return NULL;
-    if((len = rdb_load_len(fd, NULL)) == REDIS_RDB_LENERR)
+    if((len = rdb_read_store_len(fd, NULL)) == REDIS_RDB_LENERR)
         return NULL;
 
     cstr = malloc(clen);
@@ -150,7 +156,7 @@ rdb_load_lzf_string(int fd)
     }
 
     int ret;
-    if ((ret = crc_read(fd, cstr, clen)) == 0) goto err;
+    if ((ret = rdb_crc_read(fd, cstr, clen)) == 0) goto err;
     if( (ret = lzf_decompress(cstr, clen, str, len)) == 0) goto err;
     str[len] = '\0';
 
@@ -163,26 +169,26 @@ err:
     return NULL;
 }
 
-char *
-rdb_load_string(int fd)
+static char *
+rdb_read_string(int fd)
 {
     uint8_t is_encoded;
     uint32_t len;
     char *buf;
     int32_t val;
 
-    len = rdb_load_len(fd, &is_encoded);
+    len = rdb_read_store_len(fd, &is_encoded);
     if (is_encoded) {
         switch (len) {
 
             case REDIS_RDB_ENC_INT8:
             case REDIS_RDB_ENC_INT16:
             case REDIS_RDB_ENC_INT32:
-                val = rdb_load_int(fd, len);
+                val = rdb_read_int(fd, len);
                 return ll2string(val);
 
             case REDIS_RDB_ENC_LZF:
-                return rdb_load_lzf_string(fd);
+                return rdb_read_lzf_string(fd);
 
              default:
                 // unkonwn error.
@@ -195,7 +201,7 @@ rdb_load_string(int fd)
     if (!buf) {
         logger(ERROR, "Exited, as malloc failed at load string.\n");
     }
-    while ( i < len && (bytes = crc_read(fd, buf + i, len - i))) {
+    while ( i < len && (bytes = rdb_crc_read(fd, buf + i, len - i))) {
         i += bytes;
     }
     buf[len] = '\0';
@@ -203,101 +209,181 @@ rdb_load_string(int fd)
     return buf;
 }
 
-void
-push_list (lua_State *L, int fd)
+// ================================== SCRIPT PUSH UTIL. ==================================== //
+
+static void
+rdb_push_list (lua_State *L, int fd)
 {
     int i, len;
     char *elem;
 
-    len = rdb_load_len(fd, NULL); 
+    len = rdb_read_store_len(fd, NULL); 
     for (i = 0; i < len; i++) {
-        elem = rdb_load_string(fd);
+        elem = rdb_read_string(fd);
         script_push_list_elem(L, elem, i);
         free(elem);
     }
 }
 
-void
-push_hash (lua_State *L, int fd)
+static void
+rdb_push_hash (lua_State *L, int fd)
 {
     int i, len;
     char *key , *val;
 
-    len = rdb_load_len(fd, NULL); 
+    len = rdb_read_store_len(fd, NULL); 
     for (i = 0; i < len; i++) {
-        key = rdb_load_string(fd);
-        val = rdb_load_string(fd);
+        key = rdb_read_string(fd);
+        val = rdb_read_string(fd);
         script_pushtablestring(L, key, val);
         free(key);
         free(val);
     }
 }
 
-void
-rdb_load_value(lua_State *L, int fd, int type)
+// ================================== RDB LOAD. ==================================== //
+
+static void
+rdb_set_value_type(lua_State *L, int type)
+{
+    char *val_type = NULL;
+    switch (type) {
+        case REDIS_RDB_STRING:       val_type = "string"; break;
+        case REDIS_RDB_INTSET:       val_type = "set";    break;
+        case REDIS_RDB_LIST_ZIPLIST: val_type = "list";   break;
+        case REDIS_RDB_ZIPMAP:       val_type = "hash";   break;
+        case REDIS_RDB_ZSET_ZIPLIST: val_type = "zset";   break;
+        case REDIS_RDB_HASH_ZIPLIST: val_type = "hash";   break;
+        case REDIS_RDB_LIST:         val_type = "list";   break;
+        case REDIS_RDB_SET:          val_type = "set";    break;
+        case REDIS_RDB_HASH:         val_type = "hash";   break;
+        case REDIS_RDB_ZSET:         val_type = "zset";   break;
+        default: return;
+    }
+
+    script_pushtablestring(L, TYPE_FIELD_STR, val_type);
+}
+
+static void
+rdb_load_str_value(lua_State *L, int fd)
+{
+    char *str;
+
+    str = rdb_read_string(fd);
+    script_pushtablestring(L, VAL_FIELD_STR, str);
+
+    free(str);
+}
+
+static void
+rdb_load_intset_value(lua_State *L, int fd)
 {
     int i;
     int64_t v64;
-    char *str = NULL, *val_type = "string";
-    char *s64;
+    char *str, *s64; 
 
-    if (REDIS_RDB_STRING == type) {
-        str = rdb_load_string(fd);
-        script_pushtablestring(L, "value", str);
-        script_pushtablestring(L, "type", val_type);
-        free(str);
-        return;
-    }
-    
-    lua_pushstring(L, "value");
+    str = rdb_read_string(fd);
+    intset *is = (intset*) str;
+
+    lua_pushstring(L, VAL_FIELD_STR);
     lua_newtable(L);
-    if (REDIS_RDB_INTSET == type) {
-        str = rdb_load_string(fd);
-        intset *is = (intset*) str;
-        for (i = 0; i< is->length; i++) {
-            intset_get(is, i, &v64);
-            s64 = ll2string(v64); 
-            script_push_list_elem(L, s64, i);
-            free(s64);
-        }
-
-        val_type = "set";
-    } else if (REDIS_RDB_LIST_ZIPLIST == type) {
-        str = rdb_load_string(fd);
-        push_ziplist_list_or_set(L, str);
-        val_type = "list";
-    } else if (REDIS_RDB_ZIPMAP == type) {
-        str = rdb_load_string(fd);
-        push_zipmap(L, str);
-        val_type = "hash";
-    } else if (REDIS_RDB_ZSET_ZIPLIST== type
-            || REDIS_RDB_HASH_ZIPLIST == type) {
-        str = rdb_load_string(fd);
-        push_ziplist_hash_or_zset(L, str);
-        val_type = REDIS_RDB_ZSET_ZIPLIST== type? "zset" : "hash";
-    } else if (REDIS_RDB_LIST == type
-            || REDIS_RDB_SET == type) {
-        push_list(L, fd);
-        val_type = REDIS_RDB_LIST == type? "list" : "set";
-    } else if (REDIS_RDB_HASH == type
-            || REDIS_RDB_ZSET == type) {
-        push_hash(L, fd);
-        val_type = REDIS_RDB_HASH == type? "hash" : "zset";
+    for (i = 0; i< is->length; i++) {
+        intset_get(is, i, &v64);
+        s64 = ll2string(v64); 
+        script_push_list_elem(L, s64, i);
+        free(s64);
     }
-
     lua_settable(L,-3);
-    script_pushtablestring(L, "type", val_type);
 
-    if(str) free(str);
+    free(str);
 }
 
-uint32_t
+static void
+rdb_load_zllist_value(lua_State *L, int fd)
+{
+    char *str;
+
+    str = rdb_read_string(fd);
+    lua_pushstring(L, VAL_FIELD_STR);
+    lua_newtable(L);
+    push_ziplist_list_or_set(L, str);
+    lua_settable(L,-3);
+
+    free(str);
+}
+
+static void
+rdb_load_zipmap_value (lua_State *L, int fd)
+{
+    char *str;
+
+    str = rdb_read_string(fd);
+    lua_pushstring(L, VAL_FIELD_STR);
+    lua_newtable(L);
+    push_zipmap(L, str);
+    lua_settable(L,-3);
+
+    free(str);
+}
+
+static void
+rdb_load_ziplist_value(lua_State *L, int fd)
+{
+    char *str;
+
+    str = rdb_read_string(fd);
+    lua_pushstring(L, VAL_FIELD_STR);
+    lua_newtable(L);
+    push_ziplist_hash_or_zset(L, str);
+    lua_settable(L,-3);
+
+    free(str);
+}
+
+static void
+rdb_load_list_or_set_value(lua_State *L, int fd)
+{
+    lua_pushstring(L, VAL_FIELD_STR);
+    lua_newtable(L);
+    rdb_push_list(L, fd);
+    lua_settable(L,-3);
+}
+
+static void
+rdb_load_hash_or_zset_value (lua_State *L, int fd)
+{
+    lua_pushstring(L, VAL_FIELD_STR);
+    lua_newtable(L);
+    rdb_push_hash(L, fd);
+    lua_settable(L,-3);
+}
+
+void
+rdb_load_value(lua_State *L, int fd, int type)
+{
+    switch (type) {
+        case       REDIS_RDB_STRING: rdb_load_str_value(L, fd); break;
+        case       REDIS_RDB_INTSET: rdb_load_intset_value(L, fd); break;
+        case REDIS_RDB_LIST_ZIPLIST: rdb_load_zllist_value(L, fd); break;
+        case       REDIS_RDB_ZIPMAP: rdb_load_zipmap_value(L, fd); break;
+        case REDIS_RDB_ZSET_ZIPLIST:
+        case REDIS_RDB_HASH_ZIPLIST: rdb_load_ziplist_value(L, fd); break;
+        case         REDIS_RDB_LIST:
+        case          REDIS_RDB_SET: rdb_load_list_or_set_value(L, fd); break;
+        case         REDIS_RDB_HASH:
+        case         REDIS_RDB_ZSET: rdb_load_hash_or_zset_value(L, fd); break;
+
+        default: logger(ERROR, "Value type error."); break;
+    }
+}
+
+static uint32_t
 rdb_load_time(int fd)
 {
     char buf[4];
     uint32_t t32;
 
-    if( crc_read(fd, buf, 4) == 0) {
+    if( rdb_crc_read(fd, buf, 4) == 0) {
         logger(ERROR, "Exited, as read error on load time.\n");
     }
     memcpy(&t32, buf, 4);
@@ -305,13 +391,13 @@ rdb_load_time(int fd)
     return t32;
 }
 
-uint64_t
+static uint64_t
 rdb_load_ms_time(int fd)
 {
     char buf[8];
     uint64_t t64;
 
-    if( crc_read(fd, buf, 8) == 0) {
+    if( rdb_crc_read(fd, buf, 8) == 0) {
         logger(ERROR, "Exited, as read error on load microtime.\n");
     }
     memcpy(&t64, buf, 8);
@@ -328,7 +414,7 @@ rdb_load(lua_State *L, const char *path)
 
     int rdb_fd = open(path, O_RDONLY);
     // read magic string(5bytes) and version(4bytes)
-    if(crc_read(rdb_fd, buf, 9) == 0) {
+    if(rdb_crc_read(rdb_fd, buf, 9) == 0) {
         logger(ERROR,"Exited, as read error on laod version\n");
     }
     buf[9] = '\0';
@@ -336,34 +422,33 @@ rdb_load(lua_State *L, const char *path)
     version = atoi(buf + 5);
 
     lua_getglobal(L, RDB_ENV);
-    script_pushtableinteger(L, "version", version);
+    script_pushtableinteger(L, VERSION_STR, version);
     lua_pop(L,-1);
 
 
     while (1) {
         expire_time = -1;
-        type = rdb_load_type(rdb_fd);
+        type = rdb_read_kv_type(rdb_fd);
 
         // load expire time if exists
         if (REDIS_EXPIRE_SEC == type) {
-            // convert to millisecond
             expire_time = rdb_load_time(rdb_fd); 
-            // load value type
-            type = rdb_load_type(rdb_fd);
+            type = rdb_read_kv_type(rdb_fd);
         } else if (REDIS_EXPIRE_MS == type) {
             expire_time = rdb_load_ms_time(rdb_fd) / 1000; 
-            // load value type
-            type = rdb_load_type(rdb_fd);
+            type = rdb_read_kv_type(rdb_fd);
         }
 
         // select db
         if (REDIS_SELECT_DB == type) {
-            if (crc_read(rdb_fd, buf, 1) == 0) {
+            if (rdb_crc_read(rdb_fd, buf, 1) == 0) {
                 logger(ERROR, "Exited, as read error on laod db num.\n");
             }
+
             db_num = (uint8_t)buf[0];
             lua_getglobal(L, RDB_ENV);
-            script_pushtableinteger(L, "db_num", db_num);
+            // set db num
+            script_pushtableinteger(L, DB_NUM_STR, db_num);
             lua_pop(L,-1);
             continue;
         }
@@ -372,18 +457,19 @@ rdb_load(lua_State *L, const char *path)
         if(REDIS_EOF == type) break;
 
         // read key
-        char *key = rdb_load_string(rdb_fd);
+        char *key = rdb_read_string(rdb_fd);
         if (key) {
             lua_getglobal(L, RDB_CB); 
             lua_newtable(L);
-            script_pushtablestring(L, "key", key);
-            script_pushtableinteger(L, "expire_time", expire_time);
+            script_pushtablestring(L, KEY_FIELD_STR, key);
+            script_pushtableinteger(L, EXP_FIELD_STR, expire_time);
 
             // read value
             rdb_load_value(L, rdb_fd, type);
+            // set value type
+            rdb_set_value_type(L, type);
             if( lua_pcall(L, 1, 0, 0) != 0 ) {
-                logger(ERROR, "Runing handle function failed: %s",
-                        lua_tostring(L, -1));
+                logger(ERROR, "Runing handle function failed: %s", lua_tostring(L, -1));
             }
                 
         }
